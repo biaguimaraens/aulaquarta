9d835d8433796750c5ae283e842ca7384ce63fc4
bianca <biaguimaraens@gmail.com>
2019-01-30 11:58:37 -0200

    aula deu certo

--- app/Http/Controllers/EmprestimosController.php
+++ app/Http/Controllers/EmprestimosController.php
@@ -6,17 +6,85 @@ use Illuminate\Http\Request;
 
 use App\emprestimos;
 
+use App\Http\Resources\EmprestimosResource;
+
 class EmprestimosController extends Controller
 {
-  public function create(Request $request){
-    $emprestimos = new emprestimos;
-    $emprestimos->status = $request->status;
-    $emprestimos->dataDeInicio = $request->dataDeInicio;
-    $emprestimos->dataDeTermino = $request->dataDeTermino;
-    $emprestimos->anoDeLancamento = $request->anoDeLancamento;
-    $emprestimos->idCliente = $request->idCliente;
-    $emprestimos->idLivro = $request->idLivro;
-    $emprestimos->save();
-    return response()->json([$emprestimos]);
-  }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      return EmprestimosResource::collection(emprestimos::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $emprestimo = new emprestimos;
+      $emprestimo->status = $request->status;
+      $emprestimo->dataDeInicio = $request->dataDeInicio;
+      $emprestimo->dataDeTermino = $request->dataDeTermino;
+      $emprestimo->anoDeLancamento = $request->anoDeLancamento;
+      $emprestimo->idCliente = $request->idCliente;
+      $emprestimo->idLivro = $request->idLivro;
+      $emprestimo->save();
+      return new EmprestimosResource($emprestimo);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+      $emprestimo = emprestimos::findOrFail($id);
+      return new EmprestimosResource($emprestimo);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $emprestimo = emprestimos::findOrFail($id);
+      if($request->status)
+        $emprestimo->status = $request->status;
+      if($request->dataDeInicio)
+        $emprestimo->dataDeInicio = $request->dataDeInicio;
+      if($request->dataDeTermino)
+        $emprestimo->dataDeTermino = $request->dataDeTermino;
+      if($request->anoDeLancamento)
+        $emprestimo->anoDeLancamento = $request->anoDeLancamento;
+      if($request->idCliente)
+        $emprestimo->idCliente = $request->idCliente;
+      if($request->idLivro)
+        $emprestimo->idLivro = $request->idLivro;
+      return new EmprestimosResource($emprestimo);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      emprestimos::destroy($id);
+      return response()->json(['DELETADO']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/clients', 'ClientsController@create');
-Route::post('/livros', 'LivrosController@create');
-Route::post('/emprestimos', 'EmprestimosController@create');
-Route::get('/clients', 'ClientsController@list');
-Route::get('/clients/{id}', 'ClientsController@show');
-Route::put('/clients/{id}', 'ClientsController@update');
-Route::delete('/clients/{id}', 'ClientsController@delete');
+Route::post('/clients', 'ClientsController@create')->name('create_clients');
+Route::post('/livros', 'LivrosController@create')->name('create_livros');
+Route::get('/clients', 'ClientsController@list')->name('list_clients');
+Route::get('/clients/{id}', 'ClientsController@show')->name('show_clients');
+Route::put('/clients/{id}', 'ClientsController@update')->name('update_clients');
+Route::delete('/clients/{id}', 'ClientsController@delete')->name('delete_clients');
+
+Route::apiResource('emprestimos','EmprestimosController');
